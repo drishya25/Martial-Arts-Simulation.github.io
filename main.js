bf284f28819fb06f2531becf70e1f00fd9751b5a
drishya25 <55805869+drishya25@users.noreply.github.com>
2021-04-29 15:16:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -56,8 +56,7 @@ let animate=(ctxt,images,animation,callback)=>{
                  audio=new Audio("kick.mp3");
             }
             ctxt.drawImage(image,x,y,400,400);
-            audio.play();
-            audio.stop();
+           
             
         }, index*100);
     });
