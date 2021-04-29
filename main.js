fd6d074be763f77e159bf4f732545e95d0f301ed
drishya25 <55805869+drishya25@users.noreply.github.com>
2021-04-29 13:04:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ let loadImages=(callback)=>{
     });  
   });
 };
-let x=400, y=900;
+let x=400, y=1200;
 let animate=(ctxt,images,animation,callback)=>{
     images[animation].forEach((image,index)=>{
         setTimeout(()=>{
